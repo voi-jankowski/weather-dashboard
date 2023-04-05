4302cbb8d4e410f7776132523c3866355634df7d
Voi <wojtek.wacek.dola@gmail.com>
2023-04-05 16:26:41 +1000

    Reviewed the code and changed var into const and let

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // GLOBAL VARIABLES
 
-var recentSearches = $("#recent-searches");
-var weatherUnits = "metric";
+const recentSearches = $("#recent-searches");
+let weatherUnits = "metric";
 
 // DAYJS GLOBAL
 // dayjs plugins
@@ -31,9 +31,9 @@ $(".btn-toggle").click(function () {
 function getLocation() {
   // Clear possible previous display of search options
   $(".removable").remove();
-  var searchPhrase = $("#search-phrase").val();
+  let searchPhrase = $("#search-phrase").val();
 
-  var locationUrl =
+  const locationUrl =
     "https://api.openweathermap.org/geo/1.0/direct?q=" +
     searchPhrase +
     "&limit=5&appid=61a66bab5454a1423d5dd78c2e92913e";
@@ -45,25 +45,25 @@ function getLocation() {
     .then(function (data) {
       // If there are no results for that search phrase display no result warning.
       if ($.isEmptyObject(data)) {
-        var createWarning = $("<h5></h5>")
+        let createWarning = $("<h5></h5>")
           .addClass("removable")
           .css({ "padding-top": "2rem" })
           .text("No results for that search. Try a different name!");
         $("#search-box").append(createWarning);
       } else {
         //   Creating display of results for user to choose desired location.
-        var createInstruction = $("<h3></h3>").text(
+        let createInstruction = $("<h3></h3>").text(
           "Select the location from below:"
         );
         $(createInstruction).addClass("removable");
         $("#search-box").append(createInstruction);
-        var createList = $("<div></div>").addClass(
+        let createList = $("<div></div>").addClass(
           "list-group location-searches removable"
         );
         $("#search-box").append(createList);
         // Display selection of results as selectable buttons with data attributes with coordinates of the location.
         $.each(data, function () {
-          var createChoiceBtns = $("<button></button>")
+          let createChoiceBtns = $("<button></button>")
             .attr("type", "button")
             .addClass("list-group-item btn btn-outline-primary removable")
             .attr("data-lat", this.lat.toFixed(2))
@@ -78,8 +78,8 @@ function getLocation() {
 
 // Fetching coordinates from data attributes of the selected location.
 function getCoordinates(event) {
-  var lat = $(event.target).data("lat").toString();
-  var lon = $(event.target).data("lon").toString();
+  let lat = $(event.target).data("lat").toString();
+  let lon = $(event.target).data("lon").toString();
   getWeather(lat, lon);
 }
 
@@ -235,16 +235,17 @@ function displayWeather(data, weatherUnits) {
   // Display the cards in case they were not visible on loading the page.
   $("#forecast-area").css({ visibility: "visible" });
   // SEPARATE FORECAST FOR DIFFERENT DAYS
+
   // Setting variables and object
-  var timezone = data.city.timezone;
-  var firstDayForecast = [];
-  var secondDayForecast = [];
-  var thirdDayForecast = [];
-  var fourthDayForecast = [];
-  var fifthDayForecast = [];
+  let timezone = data.city.timezone;
+  const firstDayForecast = [];
+  const secondDayForecast = [];
+  const thirdDayForecast = [];
+  const fourthDayForecast = [];
+  const fifthDayForecast = [];
+
   // Get the dates for each day of the forecast
   var firstDay = dayjs.unix(data.list[0].dt).utc().add(timezone, "second");
-  console.log(firstDay.format(printFormat));
   var secondDay = dayjs.unix(data.list[8].dt).utc().add(timezone, "second");
   var thirdDay = dayjs.unix(data.list[16].dt).utc().add(timezone, "second");
   var fourthDay = dayjs.unix(data.list[24].dt).utc().add(timezone, "second");
@@ -267,29 +268,22 @@ function displayWeather(data, weatherUnits) {
 
   firstDayForecast.push(data.list[0]);
 
-  console.log(data);
-  console.log(firstDayForecast);
-  console.log();
-  console.log();
-  console.log();
-  console.log(fifthDayForecast);
-
   // FUNCTIONS FOR SELECTING WEATHER DATA TO DISPLAY
   // Function for selecting the weather icon for a given day
   function getWeatherIcon(dayForecast, iconImg) {
     // collect the weather icon code from the given day in an array.
-    var weather = [];
+    const weather = [];
     // Collect only day weather icons from the forecast
     $.each(dayForecast, function () {
-      var weatherId = this.weather[0].icon;
+      let weatherId = this.weather[0].icon;
       if (weatherId.includes("d")) {
         weather.push(weatherId);
       }
     });
     // If no day weather icons, collect the last evening icon
     if (weather.length === 0) {
-      var eveningForecast = dayForecast.pop();
-      var weatherId = eveningForecast.weather[0].icon;
+      let eveningForecast = dayForecast.pop();
+      let weatherId = eveningForecast.weather[0].icon;
       weather.push(weatherId);
     }
     // Find the most frequent icon in weather array. Solution sourced at https://amjustsam.medium.com/how-to-find-most-frequent-item-of-an-array-12015df68c65
@@ -362,16 +356,16 @@ function displayWeather(data, weatherUnits) {
   // Function for getting the min and max tem for the given day
   function getTemeratures(dayForecast, tempEl) {
     // collect the temps from the given day
-    var temperatures = [];
+    let temperatures = [];
     $.each(dayForecast, function () {
       // Convert the values into integers
-      var temp = Math.round(this.main.temp);
+      let temp = Math.round(this.main.temp);
       temperatures.push(temp);
     });
 
     // Get the highest and lowest temepatures from the temperatures array
-    var highTemp = Math.max(...temperatures);
-    var lowTemp = Math.min(...temperatures);
+    let highTemp = Math.max(...temperatures);
+    let lowTemp = Math.min(...temperatures);
 
     // display them
     if (weatherUnits === "imperial") {
@@ -384,22 +378,22 @@ function displayWeather(data, weatherUnits) {
   // Function for getting the humidity on the given day
   function getHumidity(dayForecast, humidEl) {
     // collect the humidity data from the given day array
-    var humidity = [];
+    let humidity = [];
     $.each(dayForecast, function () {
-      var humid = Math.round(this.main.humidity);
+      let humid = Math.round(this.main.humidity);
       humidity.push(humid);
     });
 
     // Calulate average humidity for the day
-    var total = 0;
-    var count = 0;
+    let total = 0;
+    let count = 0;
 
     $.each(humidity, function () {
       total += this;
       count++;
     });
     // Get the integer of calculated average
-    var humidAverage = Math.round(total / count);
+    let humidAverage = Math.round(total / count);
 
     // Display the average
     humidEl.text(" " + humidAverage + "%");
@@ -407,14 +401,14 @@ function displayWeather(data, weatherUnits) {
 
   // Create function for getting the wind speen on the given day
   function getWindSpeed(dayForecast, windEl) {
-    var windSpeeds = [];
+    let windSpeeds = [];
     $.each(dayForecast, function () {
-      var wind = Math.round(this.wind.speed);
+      let wind = Math.round(this.wind.speed);
       windSpeeds.push(wind);
     });
 
     // Get the highest windspeed forecast for the day
-    var highWind = Math.max(...windSpeeds);
+    let highWind = Math.max(...windSpeeds);
 
     // Display the wind
     if (weatherUnits === "imperial") {
@@ -427,118 +421,118 @@ function displayWeather(data, weatherUnits) {
   // DISPLAYING DATA FOR FIRST DAY
 
   // Display the day of the week
-  var day1 = firstDay.format("dddd");
+  let day1 = firstDay.format("dddd");
   $("#day1-date").text(day1);
 
   // Display the icon of the weather
-  var day1Icon = $("#day1-icon");
+  let day1Icon = $("#day1-icon");
   getWeatherIcon(firstDayForecast, day1Icon);
 
   // Display min and max temp
-  var day1Temp = $("#day1-temp");
+  let day1Temp = $("#day1-temp");
   getTemeratures(firstDayForecast, day1Temp);
 
   // Display humidity
-  var day1Humid = $("#day1-humid");
+  let day1Humid = $("#day1-humid");
   getHumidity(firstDayForecast, day1Humid);
 
   // Display wind speed
-  var day1Wind = $("#day1-wind");
+  let day1Wind = $("#day1-wind");
   getWindSpeed(firstDayForecast, day1Wind);
 
   // DISPLAYING DATA FOR SECOND DAY
   // Display the day of the week
-  var day2 = secondDay.format("dddd");
+  let day2 = secondDay.format("dddd");
   $("#day2-date").text(day2);
 
   // Display the icon of the weather
-  var day2Icon = $("#day2-icon");
+  let day2Icon = $("#day2-icon");
   getWeatherIcon(secondDayForecast, day2Icon);
 
   // Display min and max temp
-  var day2Temp = $("#day2-temp");
+  let day2Temp = $("#day2-temp");
   getTemeratures(secondDayForecast, day2Temp);
 
   // Display humidity
-  var day2Humid = $("#day2-humid");
+  let day2Humid = $("#day2-humid");
   getHumidity(secondDayForecast, day2Humid);
 
   // Display wind speed
-  var day2Wind = $("#day2-wind");
+  let day2Wind = $("#day2-wind");
   getWindSpeed(secondDayForecast, day2Wind);
 
   // DISPLAYING DATA FOR THIRD DAY
   // Display the day of the week
-  var day3 = thirdDay.format("dddd");
+  let day3 = thirdDay.format("dddd");
   $("#day3-date").text(day3);
 
   // Display the icon of the weather
-  var day3Icon = $("#day3-icon");
+  let day3Icon = $("#day3-icon");
   getWeatherIcon(thirdDayForecast, day3Icon);
 
   // Display min and max temp
-  var day3Temp = $("#day3-temp");
+  let day3Temp = $("#day3-temp");
   getTemeratures(thirdDayForecast, day3Temp);
 
   // Display humidity
-  var day3Humid = $("#day3-humid");
+  let day3Humid = $("#day3-humid");
   getHumidity(thirdDayForecast, day3Humid);
 
   // Display wind speed
-  var day3Wind = $("#day3-wind");
+  let day3Wind = $("#day3-wind");
   getWindSpeed(thirdDayForecast, day3Wind);
 
   // DISPLAYING DATA FOR FOURTH DAY
   // Display the day of the week
-  var day4 = fourthDay.format("dddd");
+  let day4 = fourthDay.format("dddd");
   $("#day4-date").text(day4);
 
   // Display the icon of the weather
-  var day4Icon = $("#day4-icon");
+  let day4Icon = $("#day4-icon");
   getWeatherIcon(fourthDayForecast, day4Icon);
 
   // Display min and max temp
-  var day4Temp = $("#day4-temp");
+  let day4Temp = $("#day4-temp");
   getTemeratures(fourthDayForecast, day4Temp);
 
   // Display humidity
-  var day4Humid = $("#day4-humid");
+  let day4Humid = $("#day4-humid");
   getHumidity(fourthDayForecast, day4Humid);
 
   // Display wind speed
-  var day4Wind = $("#day4-wind");
+  let day4Wind = $("#day4-wind");
   getWindSpeed(fourthDayForecast, day4Wind);
 
   // DISPLAYING DATA FOR FIFTH DAY
   // Display the day of the week
-  var day5 = fifthDay.format("dddd");
+  let day5 = fifthDay.format("dddd");
   $("#day5-date").text(day5);
 
   // Display the icon of the weather
-  var day5Icon = $("#day5-icon");
+  let day5Icon = $("#day5-icon");
   getWeatherIcon(fifthDayForecast, day5Icon);
 
   // Display min and max temp
-  var day5Temp = $("#day5-temp");
+  let day5Temp = $("#day5-temp");
   getTemeratures(fifthDayForecast, day5Temp);
 
   // Display humidity
-  var day5Humid = $("#day5-humid");
+  let day5Humid = $("#day5-humid");
   getHumidity(fifthDayForecast, day5Humid);
 
   // Display wind speed
-  var day5Wind = $("#day5-wind");
+  let day5Wind = $("#day5-wind");
   getWindSpeed(fifthDayForecast, day5Wind);
 }
 
 // Save the search in the local storage.
 function saveCity(lat, lon, city) {
-  var newSearch = {
+  let newSearch = {
     lat,
     lon,
     city,
   };
-  var savedSearch = JSON.parse(localStorage.getItem("savedSearch")) || [];
+  let savedSearch = JSON.parse(localStorage.getItem("savedSearch")) || [];
 
   // Check if the object with those coordinates already exists
   if (
@@ -548,7 +542,7 @@ function saveCity(lat, lon, city) {
     )
   ) {
     // Get the index number of the object if that object exists
-    var arrayIndex = savedSearch.findIndex(
+    let arrayIndex = savedSearch.findIndex(
       (savedSearch) =>
         savedSearch.lat === newSearch.lat && savedSearch.lon === newSearch.lon
     );
@@ -576,20 +570,20 @@ function renderSearches() {
   // Clear the recent-searches cards if any are displayed.
   recentSearches.empty();
   // Retrieve the recent searches
-  var savedSearch = JSON.parse(localStorage.getItem("savedSearch"));
+  let savedSearch = JSON.parse(localStorage.getItem("savedSearch"));
   // Check if data is returned, if not exit out of the function
   if (savedSearch !== null) {
     $.each(savedSearch, function () {
-      var div1 = $("<div>")
+      let div1 = $("<div>")
         .addClass("card recent-card")
         .css({ width: "20rem", height: "5rem", "flex-grow": "1" });
-      var div2 = $("<div>")
+      let div2 = $("<div>")
         .addClass("card-body text-center d-flex align-items-center")
         .attr({
           "data-lat": this.lat,
           "data-lon": this.lon,
         });
-      var h3 = $("<h3>")
+      let h3 = $("<h3>")
         .addClass("card-title")
         .attr({
           "data-lat": this.lat,
@@ -617,8 +611,8 @@ function getLocalCoordinates() {
 }
 // Save the local coorinates as parameters for getWeather to display the local weather upon loading the page.
 function getPosition(position) {
-  var localLat = position.coords.latitude.toFixed(2);
-  var localLon = position.coords.longitude.toFixed(2);
+  let localLat = position.coords.latitude.toFixed(2);
+  let localLon = position.coords.longitude.toFixed(2);
 
   getWeather(localLat, localLon);
 }
